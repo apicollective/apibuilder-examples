4d3f9248ba0c0d27897943f1437927676fce7511
Michael Bryzek <mike@flow.io>
2020-09-14 13:14:40 -0400

    Invoke both generators

--- graphql/users/generate.sh
+++ graphql/users/generate.sh
@@ -1,5 +1,11 @@
+echo "Invoking the 'graphql' code generator:"
+
 # If you only want the GraphQL Schema:
-# apibuilder code apicollective examples-graphql-users latest graphql_apollo ./generated
+apibuilder code apicollective examples-graphql-users latest graphql ./generated
+
+
+echo ""
+echo "Invoking the 'graphql_apollo' code generator:"
 
 # Or if you want the GraphQL Schema and TypeScript adapters for Apollo Server:
 apibuilder code apicollective examples-graphql-users latest graphql_apollo ./generated
